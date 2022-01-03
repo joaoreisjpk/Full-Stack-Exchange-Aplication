8316b1edca7e2481df2c4aacb252012e4247f175
João Reis <joaoreisjpk@gmail.com>
2022-01-03 13:22:29 -0300

    fazendo a pagina de login

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
 import Dashboard from './pages/Dashboard';
 import History from './pages/History';
 import Login from './pages/Login';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/history' element={<History />} />
+      </Routes>
+    </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form } from 'formik';
 
-export default function Login() {
+import { Button, TextField, Typography, Grid, Stack, Box } from '@mui/material';
+
+import SendIcon from '@mui/icons-material/Send';
+import { useNavigate } from 'react-router-dom';
+import MUInput from '../components/MUInput';
+
+export default function Home() {
+  const navigate = useNavigate();
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  const validate = ({
+    password,
+    email,
+  }: {
+    password: string;
+    email: string;
+  }) => {
+    if (!emailRegex.test(email)) {
+      return { email: 'The email format must be example@email.com' };
+    } else if (password.length < 6) {
+      return { password: 'The password must have at least 6 characters' };
+    }
+    return {};
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <Box>
+      <Formik
+        initialValues={{ password: '', email: '' }}
+        validate={({ password, email }) => validate({ password, email })}
+        onSubmit={() => navigate('dashboard')}
+      >
+        {() => (
+          <Form>
+            <Grid
+              container
+              marginTop='3rem'
+              gap={8}
+              justifyItems='center'
+              direction='column'
+              justifyContent='center'
+              alignItems='center'
+            >
+              <Typography
+                variant='h3'
+                component='label'
+                htmlFor='email'
+                marginBottom='20px'
+              >
+                LOGIN
+              </Typography>
+              <Stack spacing={2}>
+                <MUInput name='email' type='email' label='E-Mail' />{' '}
+                <MUInput name='password' type='password' label='Password' />{' '}
+                <Button
+                  endIcon={<SendIcon />}
+                  type='submit'
+                  size='large'
+                  variant='contained'
+                  sx={{
+                    width: 265,
+                  }}
+                >
+                  Enviar
+                </Button>
+              </Stack>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
 }
