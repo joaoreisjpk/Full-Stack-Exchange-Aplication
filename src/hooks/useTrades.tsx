74584cb34a9cc3a5491aa074263d0881b57f06eb
João Reis <joaoreisjpk@gmail.com>
2022-01-03 13:45:57 -0300

    creating a personalized hook

---
+++ src/hooks/useTrades.tsx
@@ -0,0 +1,32 @@
+import { Children, createContext, useContext, useEffect, useState } from "react";
+
+interface IContext {
+  trades: string[];
+  setTrades: (param: string[]) => void;
+}
+
+export const TradesContext = createContext({} as IContext);
+
+export function TradesProvider({children}) {
+  const [trades, setTrades] = useState([] as string[]);
+
+  useEffect(() => {
+    const getStorage = localStorage.getItem('trades') || '[]'
+    setTrades(JSON.parse(getStorage));
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('trades', JSON.stringify(trades))
+  }, [trades])
+
+  return (
+    <TradesContext.Provider value={{ trades, setTrades }}>
+      {children}
+    </TradesContext.Provider>
+  )
+}
+
+export function useTrades() {
+  const context = useContext(TradesContext);
+  return context
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TradesProvider } from './hooks/useTrades';
 import Dashboard from './pages/Dashboard';
 import History from './pages/History';
 import Login from './pages/Login';
 
 ReactDOM.render(
+  <TradesProvider>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/history' element={<History />} />
       </Routes>
-    </BrowserRouter>,
+    </BrowserRouter>
+  </TradesProvider>,
   document.getElementById('root')
 );
