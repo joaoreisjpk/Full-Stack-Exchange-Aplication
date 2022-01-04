4045b9296abf80f46aade5b8e1129c08d4f02ddc
João Reis <joaoreisjpk@gmail.com>
2022-01-04 08:53:08 -0300

    doging the api page

---
+++ src/api/currencyAPI.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const key = 'UNPEHGNOR7XNN6LO';
+
+const api = axios.create({
+  baseURL: 'https://www.alphavantage.co/'
+})
+
+const calculateCurrencyExchange = async (baseCurrency, exchangeCurrency) => {
+  const response = await api
+    .get(`query?function=CURRENCY_EXCHANGE_RATE&from_currency=${baseCurrency}&to_currency=${exchangeCurrency}&apikey=${key}`);
+
+  return response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+}
+
+export { calculateCurrencyExchange }
